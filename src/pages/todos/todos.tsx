a5415cc9ae341aa5643ab1741634ef610a0da04a
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2024-02-18 01:10:37 +0200

    Logout

--- src/pages/todos/todos.tsx
+++ src/pages/todos/todos.tsx
@@ -3,10 +3,20 @@ import { Todo } from '@/models/todo';
 import AddTodo from '@/components/AddTodo';
 import ListTodos from '@/components/ListTodos';
 import withAuth from '@/utils/withAuth';
+import { useAuth } from '@/context/AuthContext';
+import { useRouter } from 'next/router';
 
 const Todos = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [filter, setFilter] = useState('all');
+
+  const auth = useAuth();
+  const router = useRouter();
+
+  const handleLogout = () => {
+    auth?.logout();
+    router.push('/auth/login');
+  };
 
   const buttonBaseStyle =
     'flex-1 py-2 px-4 text-sm font-medium rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500';
@@ -15,6 +25,14 @@ const Todos = () => {
 
   return (
     <div className='container mx-auto p-4'>
+      <div className='absolute top-4 right-4'>
+        <button
+          onClick={handleLogout}
+          className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500'
+        >
+          Logout
+        </button>
+      </div>
       <div className='flex flex-col items-center mb-4'>
         <h1 className='text-3xl font-bold text-gray-800 mb-4'>Todos</h1>
         <div className='w-full sm:w-1/2 lg:w-1/3 xl:w-1/4'>
