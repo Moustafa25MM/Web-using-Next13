897962ec5a67e80ee45ea7f2cff23ba823a4f271
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2024-02-18 01:20:41 +0200

    modify todos background styles

--- src/pages/todos/todos.tsx
+++ src/pages/todos/todos.tsx
@@ -24,48 +24,50 @@ const Todos = () => {
   const buttonInactiveStyle = 'text-gray-700 bg-gray-100 hover:bg-gray-200';
 
   return (
-    <div className='container mx-auto p-4'>
-      <div className='absolute top-4 right-4'>
-        <button
-          onClick={handleLogout}
-          className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500'
-        >
-          Logout
-        </button>
-      </div>
-      <div className='flex flex-col items-center mb-4'>
-        <h1 className='text-3xl font-bold text-gray-800 mb-4'>Todos</h1>
-        <div className='w-full sm:w-1/2 lg:w-1/3 xl:w-1/4'>
-          <AddTodo todos={todos} setTodos={setTodos} />
+    <div className='min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500'>
+      <div className='container mx-auto p-4 pt-10'>
+        <div className='flex justify-between items-center mb-8'>
+          <h1 className='text-4xl font-bold text-white'>Todos</h1>
+          <button
+            onClick={handleLogout}
+            className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500'
+          >
+            Logout
+          </button>
         </div>
+        <div className='flex flex-col items-center mb-4'>
+          <div className='w-full sm:w-1/2 lg:w-1/3 xl:w-1/4'>
+            <AddTodo todos={todos} setTodos={setTodos} />
+          </div>
+        </div>
+        <div className='flex space-x-4 mb-4 justify-center'>
+          <button
+            onClick={() => setFilter('all')}
+            className={`${buttonBaseStyle} ${
+              filter === 'all' ? buttonActiveStyle : buttonInactiveStyle
+            }`}
+          >
+            All
+          </button>
+          <button
+            onClick={() => setFilter('completed')}
+            className={`${buttonBaseStyle} ${
+              filter === 'completed' ? buttonActiveStyle : buttonInactiveStyle
+            }`}
+          >
+            Completed
+          </button>
+          <button
+            onClick={() => setFilter('incompleted')}
+            className={`${buttonBaseStyle} ${
+              filter === 'incompleted' ? buttonActiveStyle : buttonInactiveStyle
+            }`}
+          >
+            Uncompleted
+          </button>
+        </div>
+        <ListTodos filter={filter} setTodos={setTodos} todos={todos} />
       </div>
-      <div className='flex space-x-4 mb-4 justify-center'>
-        <button
-          onClick={() => setFilter('all')}
-          className={`${buttonBaseStyle} ${
-            filter === 'all' ? buttonActiveStyle : buttonInactiveStyle
-          }`}
-        >
-          All
-        </button>
-        <button
-          onClick={() => setFilter('completed')}
-          className={`${buttonBaseStyle} ${
-            filter === 'completed' ? buttonActiveStyle : buttonInactiveStyle
-          }`}
-        >
-          Completed
-        </button>
-        <button
-          onClick={() => setFilter('incompleted')}
-          className={`${buttonBaseStyle} ${
-            filter === 'incompleted' ? buttonActiveStyle : buttonInactiveStyle
-          }`}
-        >
-          Uncompleted
-        </button>
-      </div>
-      <ListTodos filter={filter} setTodos={setTodos} todos={todos} />
     </div>
   );
 };
